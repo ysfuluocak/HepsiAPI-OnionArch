4d3333d9b122aa60658901e4efc3f2d4cfbce1c9
ysfuluocak <yusufziyauluocak52@gmail.com>
2024-06-16 02:30:57 +0300

    Entities Created

---
+++ src/Infrastructure/HepsiAPI.Persistence/Configuration/CategoryConfiguration.cs
@@ -0,0 +1,30 @@
+﻿using HepsiAPI.Application.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace HepsiAPI.Persistence.Configuration
+{
+    public class CategoryConfiguration : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.Property(c => c.CategoryName).HasMaxLength(255);
+        }
+    }
+
+    public class BrandConfiguration : IEntityTypeConfiguration<Brand>
+    {
+        public void Configure(EntityTypeBuilder<Brand> builder)
+        {
+            builder.Property(b => b.BrandName).HasMaxLength(255);
+        }
+    }
+
+    public class ProductConfiguration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.Property(p => p.Title).HasMaxLength(255);
+        }
+    }
+}
